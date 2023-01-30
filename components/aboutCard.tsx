f309f15ca01a987ec96b3247a651577dc7d0b58a
im4nu <victorx811@gmail.com>
2023-01-29 22:52:19 -0300

    card component updated

--- components/aboutCard.tsx
+++ components/aboutCard.tsx
@@ -1,3 +1,4 @@
+import { url } from 'inspector'
 import Image from 'next/image'
 import {
     disney,
@@ -12,56 +13,33 @@ import {
 
 export default function AboutCard(){
     const imagesCard = [
-        { img: disney }, 
-        { img: milaMoura}, 
-        { img: prespontoSite},
-        { img: grupow},
-        { img: jedigital},
-        { img: recon},
-        { img: vendas},
-        { img: presponto},
+        { img: disney, name: 'Disney World App', sub: 'App automação dos parques Disney', link: '#'}, 
+        { img: milaMoura, name: 'Mila Moura', sub: 'Divulgação de marca pessoal da Mila Moura', link: ''}, 
+        { img: prespontoSite, name: 'Pres Ponto - Site', sub: 'Divulgação do ateliê de costura Prés ponto', link: ''},
+        { img: grupow, name: 'Grupo W', sub: 'Divulgação de eventos do Grupo W', link: ''},
+        { img: jedigital, name: 'Je Digital Art', sub: 'Captação de clientes da Agência Je Digital Art', link: ''},
+        { img: recon, name: 'Recon - App', sub: 'Aplicativo para Android e IOS da catedral da reconciliação', link: ''},
+        { img: vendas, name: 'Venda Seu Carro', sub: 'Site e sistema de venda de carros', link: ''},
+        { img: presponto, name: 'Presponto - App', sub: 'Aplicativo de atendimento do ateliê Pres ponto', link: ''},
     ]
     return(
         <div className="grid w-screen justify-between grid-flow-row grid-cols-2 gap-12">
-            <button className="flex flex-col items-center text-center justify-end bg-vendas bg-cover bg-center w-[170px] h-[147px]">
-                <h2>Venda Seu Carro</h2>
-                <p className='text-xs px-1'>Site e sistema de venda de carros</p>
-            </button>
+            {
+            imagesCard.map((index) =>
+                <a 
+                    key={index.name}
+                    href={index.link}
+                    target='_blank'
+                    className="flex flex-col items-center text-center justify-end bg-cover bg-center w-[170px] h-[147px]" rel="noreferrer"
+                >
+                <Image alt={index.name} src={index.img} className='-z-20' />
 
-            <button className="flex flex-col items-center text-center justify-end bg-disney bg-cover bg-center w-[170px] h-[147px]">
-                <h2>Disney World App</h2>
-                <p className='text-xs px-1'>App automação dos parques Disney</p>
-            </button>
-
-            <button className="flex flex-col items-center text-center justify-end bg-grupow bg-cover bg-center w-[170px] h-[147px]">
-                <h2>Grupo W</h2>
-                <p className='text-xs px-1'>Divulgação de eventos do Grupo W</p>
-            </button>
-            
-            <button className="flex flex-col items-center text-center justify-end bg-jedigital bg-cover bg-center w-[170px] h-[147px]">
-                <h2>Je Digital Art</h2>
-                <p className='text-xs px-1'>Captação de clientes da Agência Je Digital Art</p>
-            </button>
-
-            <button className="flex flex-col items-center text-center justify-end bg-recon bg-cover bg-center w-[170px] h-[147px]">
-                <h2>Recon - App</h2>
-                <p className='text-xs px-1'>Aplicativo para Android e IOS da catedral da reconciliação</p>
-            </button>
-
-            <button className="flex flex-col items-center text-center justify-end bg-presponto bg-cover bg-center w-[170px] h-[147px]">
-                <h2>Pres Ponto - Site</h2>
-                <p className='text-xs px-1'>Aplicativo de atendimento do ateliê Pres ponto</p>
-            </button>
-
-            <button className="flex flex-col items-center text-center justify-end bg-mila bg-cover bg-center w-[170px] h-[147px]">
-                <h2>Mila Moura</h2>
-                <p className='text-xs px-1'>Divulgação de marca pessoal da Mila Moura</p>
-            </button>
-            
-            <button className="flex flex-col items-center text-center justify-end bg-prespontoSite bg-cover bg-center w-[170px] h-[147px]">
-                <h2>Pres Ponto</h2>
-                <p className='text-xs px-1'>Divulgação do ateliê de costura Prés ponto</p>
-            </button>
+                <div className="flex flex-col -mt-16">
+                    <h2>{index.name}</h2>
+                    <p className='text-xs px-1'>{index.sub}</p>
+                    </div>
+                </a>
+        )}
         </div>
     )
 }
--- components/button.tsx
+++ components/button.tsx
@@ -1,7 +1,7 @@
 export default function GlobalButton(){
     return(
-        <button className="flex bgButton py-4 px-14 text-center justify-center items-center text-primary-white rounded-lg">
+        <a href="#about" className="flex bgButton py-4 px-14 text-center justify-center items-center text-primary-white rounded-lg">
             Ver Sites
-        </button>
+        </a>
     )
 }
